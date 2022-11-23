033721b3b9bd4090765e8ea8c70b9884c1df5efe
bankjirapan <jirapan_yankhan@hotmail.com>
2022-11-23 18:02:27 +0700

    [EDIR] Map selector measure circle

--- web/client/selectors/mapInfo.js
+++ web/client/selectors/mapInfo.js
@@ -52,7 +52,7 @@ export const generalInfoFormatSelector = (state) => get(state, "mapInfo.configur
 export const showEmptyMessageGFISelector = (state) => get(state, "mapInfo.configuration.showEmptyMessageGFI", true);
 export const mapInfoConfigurationSelector = (state) => get(state, "mapInfo.configuration", {});
 
-export const measureActiveSelector = (state) => get(state, "controls.measure.enabled") && (get(state, "measurement.lineMeasureEnabled") || get(state, "measurement.areaMeasureEnabled") || get(state, "measurement.bearingMeasureEnabled"));
+export const measureActiveSelector = (state) => get(state, "controls.measure.enabled") && (get(state, "measurement.lineMeasureEnabled") || get(state, "measurement.areaMeasureEnabled") || get(state, "measurement.areaCircleEnabled") || get(state, "measurement.bearingMeasureEnabled"));
 /**
  * Clicked point of mapInfo
  * @param {object} state the state
